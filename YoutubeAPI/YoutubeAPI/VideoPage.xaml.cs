f0b85944aec08b87783cdcb6023d4529a2313dab
nguyencong12610 <53223955+nguyencong12610@users.noreply.github.com>
2020-08-21 11:54:48 +0700

    Pus

---
+++ YoutubeAPI/YoutubeAPI/VideoPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MyToolkit.Multimedia;
+using System;
+using System.Net.NetworkInformation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using YoutubeAPI.Model;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace YoutubeAPI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+       Video video;
+        public VideoPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                if (NetworkInterface.GetIsNetworkAvailable())
+                {
+                    video = e.Parameter as Video;
+                    var Url = await YouTube.GetVideoUriAsync(video.Id, YouTubeQuality.Quality1080P);
+                    Player.Source = Url.Uri;
+                }
+                else
+                {
+                    MessageDialog message = new MessageDialog("You are not connected to internet");
+                    await message.ShowAsync();
+                    this.Frame.GoBack();
+                }
+            }
+            catch { }
+            base.OnNavigatedTo(e);
+        }
+
+        private void btHomepage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), new object());
+        }
+    }
+}
